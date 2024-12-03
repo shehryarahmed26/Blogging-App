b78776aefa3780104512c06566f37c80cda1e587
Shehryar <shehryarahmed268@gmail.com>
2024-12-03 18:17:19 +0500

    'api-developed'

--- src/app/(root)/blogs/page.js
+++ src/app/(root)/blogs/page.js
@@ -3,19 +3,20 @@ import Header from '@/app/components/header/Header'
 import React from 'react'
 
 const Blogs = async () => {
-  let allblogs = await fetch('https://blogging-app-sh.vercel.app/api/blogs')
-  allblogs = await allblogs.json()
-  const blogs = allblogs.blogs
+  // let allblogs = await fetch('https://blogging-app-sh.vercel.app/api/blogs')
+  // allblogs = await allblogs.json()
+  // const blogs = allblogs.blogs
   return (
     <div>  
       <Header/>
       <h2 className='text-center text-2xl sm:text-4xl text-gray-950 pb-10 pt-4'>Explore Blogs</h2>
       <div className='flex flex-wrap justify-center gap-6'>
-        {
+        {/* {allblogs ? 
           blogs.map((blog) => (
             <Blogcard key={blog._id} blog={blog} />
-          ))
-        }
+          )) : */}
+          <div>No blogs available</div>
+        {/* } */}
       </div>
       </div>
   )
